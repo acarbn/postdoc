d1ad4838269420b8e83aca7f712b669a8b64d33c
acarbn <brcnacar@gmail.com>
2023-01-10 12:51:30 +0000

    initial

---
+++ GNM_complex_mono_comp.m
@@ -0,0 +1,14 @@
+%% This code compares the modes derived from the complex with the
+% allosteric modes of the monomer.
+%% 
+clear all: clc: close all:
+monomodeset=[1 3];
+monof='1OMP'; % apo structure 
+complexf1='3OSQ';
+complexf2='3OSR';
+
+[MSF1,resnum]=GNM(monof,monomodeset(1),monomodeset(1),'A');
+[MSF2,resnum]=GNM(monof,monomodeset(2),monomodeset(2),'A');
+
+% merge 173 & 427 for 3OSQ
+% merge 311 & 564 for 3OSR 
