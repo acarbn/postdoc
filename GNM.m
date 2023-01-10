40522b2780ea1b2365f242fd929fb04c39418196
acarbn <brcnacar@gmail.com>
2023-01-10 13:03:28 +0000

    seperate MSFs for each mode

--- GNM.m
+++ GNM.m
@@ -73,8 +73,8 @@ end
 	
 % gnmCfile = fopen('GNM_MSF','a');
 invcont=zeros(resnum,resnum,mode_max);
-%for kk=resnum-mode_max:resnum-mode_beg
-for kk=[resnum-mode_max resnum-mode_beg] 
+for kk=resnum-mode_max:resnum-mode_beg
+%for kk=[resnum-mode_max resnum-mode_beg] 
     for j=1:resnum
         for i=1:resnum
             invcont(i,j,resnum-kk)=invcont(i,j,resnum-kk)+U(i,kk)*U(j,kk)/w(kk);
