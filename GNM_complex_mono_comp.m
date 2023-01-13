c4491b2bfb791fbfbda206312991740414053811
acarbn <brcnacar@gmail.com>
2023-01-13 10:59:27 +0000

    better

--- GNM_complex_mono_comp.m
+++ GNM_complex_mono_comp.m
@@ -1,7 +1,7 @@
 %% This code compares the modes derived from the complex with the
 % allosteric modes of the monomer.
 %% Initialize
-clear all: clc: close all:
+clear all; clc; close all;
 monomodeset=[1 3];
 monof='1OMP'; % apo structure 
 complexf1='3OSQ';
@@ -13,5 +13,13 @@ complexf2='3OSR';
 % merge 173 & 427 for 3OSQ
 % merge 311 & 564 for 3OSR 
 
-[MSFc1,resnum]=GNM(complexf1,1,10,'A');
-[MSFc2,resnum]=GNM(complexf2,1,10,'A');
+[MSFc1,resnum2]=GNM(complexf1,1,10,'A');
+[MSFc2,resnum3]=GNM(complexf2,1,10,'A');
+
+for kk=1:10
+    MSFc1m(kk,:)=MSFc1(kk,[1:173-1 422-6-1-3:resnum2]);
+    MSFc2m(kk,:)=MSFc2(kk,[5:311+4 561-17-3+4:resnum3]);
+    ov1(kk)=dot(MSFc1m(kk,:),MSF1(2:369))/(norm(MSFc1m(kk,:))*norm(MSF1(2:369)))
+end
+
+
